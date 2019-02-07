9c3a7d4fa756ef09a0df60f7f7eadc342a01d87f
Behnam M <45321822+behysun88@users.noreply.github.com>
2019-02-07 11:37:04 -0800

    Update maximum daylight tracking system.py

--- maximum daylight tracking system.py
+++ maximum daylight tracking system.py
@@ -4,20 +4,54 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib as mpl
 import numpy as np
 import seaborn as sns
+import csv as csv
 
-#matplotlib inline
+#Data Pre-processing and importing info from the spreadsheet collected 
+#through the data logging system, Arduino
+#header = csv.reader('Experiment')
+#
+#fname = 'Experiment.csv'
+#data = pd.read_csv(fname)
+##First Column time of the day
+#daytime = data.values[:, :1]
+##Second Column vertical position
+#vertical = data.values[:, 1:2]
+##Third column horizental position
+#horizental = data.values[:,2:3]
+#fig = plt.figure(figsize = (6,4))
+#
+#xs = horizental
+#ys = vertical
+#zs = daytime
+#
+#ax = fig.add_subplot(111, projection = '3d')
+#ax.scatter(xs, ys, zs, s=50, alpha=0.6, edgecolors='w')
+#ax.set_xlabel('Vertical Vertical')
+#ax.set_ylabel('Horizental Max')
+#ax.set_zlabel('Day - 24 Hours')
+def load_data():
+    header = csv.reader('Experiment')
+    
+    fname = 'Experiment.csv'
+    data = pd.read_csv(fname)
+    #First Column time of the day
+    daytime = data.values[:, :1]
+    #Second Column vertical position
+    vertical = data.values[:, 1:2]
+    #Third column horizental position
+    horizental = data.values[:,2:3]
+    fig = plt.figure(figsize = (12,8))
+    
+    xs = horizental
+    ys = vertical
+    zs = daytime
+    
+    ax = fig.add_subplot(111, projection = '3d')
+    ax.scatter(xs, zs, ys,  s=300, alpha=1.0)   # there are different setting for scatter plot look it up
+    ax.set_xlabel('Horizental Position')
+    ax.set_ylabel('Day - 24 Hours')
+    ax.set_zlabel('Vertical Position')
 
-
-# Visualizing 3-D numeric data with Scatter Plots
-# length, breadth and depth
-fig = plt.figure(figsize=(12, 8))
-ax = fig.add_subplot(111, projection='3d')
-
-xs = wines['residual sugar']
-ys = wines['fixed acidity']
-zs = wines['alcohol']
-ax.scatter(xs, ys, zs, s=50, alpha=0.6, edgecolors='w')
-
-ax.set_xlabel('Vertical Vertical')
-ax.set_ylabel('Horizental Max')
-ax.set_zlabel('Day - 24 Hours')
+if __name__ == '__main__':
+    
+    load_data()
